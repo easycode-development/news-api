a730c4169b4c316b826abc46c6bf4637a5a3168c
dmgame <denis.m.pcspace@gmail.com>
2020-05-24 21:53:25 +0300

    add port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const NewsAPI = require('newsapi');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
